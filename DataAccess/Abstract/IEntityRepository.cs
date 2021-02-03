15e31c0134ae33e1d4f17323a7b8a4c4ea54f1a0
mcanb <mcanb@DESKTOP-TSP3G1B>
2021-02-03 23:52:19 +0300

    3.02.2021

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entites.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint //class: reference type //IEntity is not an object //new(): must be newable
+    public interface IEntityRepository<T> where T:class,IEntity,new() 
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
